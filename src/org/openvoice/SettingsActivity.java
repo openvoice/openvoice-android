f43458fd2e05ac61ae69efbee0f4240dc0212aad
Zhao Lu <zlu@zlu-imac.local>
2010-05-27 10:36:23 -0700

    update server url due to heroku app renaming to openvoice

--- src/org/openvoice/SettingsActivity.java
+++ src/org/openvoice/SettingsActivity.java
@@ -7,7 +7,7 @@ import android.preference.PreferenceManager;
 
 public class SettingsActivity extends PreferenceActivity {
 	private static final String OPT_SERVER_URL = "server_url";
-	private static final String OPT_SERVER_URL_DEFAULT = "http://tropovoice.heroku.com";
+	private static final String OPT_SERVER_URL_DEFAULT = "http://openvoice.heroku.com";
 //	private static final String OPT_SERVER_URL_DEFAULT = "http://web1.tunnlr.com:10790";
 
 	@Override
